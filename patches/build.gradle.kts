6f8c73c1388d4180e4f11612872f674839529fea
Nagelmaier-Jonas <joe.nagelmaier@gmail.com>
2026-04-13 18:48:24 +0200

    feat: update author name in build.gradle.kts

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -5,7 +5,7 @@ patches {
         name = "Studo Patches"
         description = "Patches for Studo"
         source = "git@github.com:Nagelmaier-Jonas/revanced-patches.git"
-        author = "Nagelmaier-Jonas"
+        author = "Jonas"
         contact = "N/A"
         website = "https://revanced.app"
         license = "GNU General Public License v3.0"
