f112c9f2c037304382fba2acd59d917864b9f2fe
Nagelmaier-Jonas <joe.nagelmaier@gmail.com>
2026-04-13 18:41:34 +0200

    chore: update repository source and version in patches.json

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -4,8 +4,8 @@ patches {
     about {
         name = "Studo Patches"
         description = "Patches for Studo"
-        source = "git@github.com:Vernoxvernax/revanced-patches.git"
-        author = "Vernoxvernax"
+        source = "git@github.com:Nagelmaier-Jonas/revanced-patches.git"
+        author = "Nagelmaier-Jonas"
         contact = "N/A"
         website = "https://revanced.app"
         license = "GNU General Public License v3.0"
